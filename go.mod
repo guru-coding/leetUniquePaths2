6dea584a0a9693281e9fd6b943b61883d652149a
Ruslan Gusmanov <gusmanovruslan@gmail.com>
2023-12-23 18:37:35 +0300

    Initial DP-solution and example tests
    LeetCode study plan: Dynamic Programming
    Problem #63: Unique Paths II
    
    DP-solution: bottom-up forward
    Time complexity: O(n^2)
    Space complexity: O(n^2)

---
+++ go.mod
@@ -0,0 +1,3 @@
+module leetUniquePaths2
+
+go 1.21
---
+++ leetUniquePaths2.go
@@ -0,0 +1,38 @@
+package leetUniquePaths2
+
+func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+	var m, n int
+	m = len(obstacleGrid)
+	if m > 0 {
+		n = len(obstacleGrid[0])
+	}
+	dp := make([][]int, m)
+	var i, j int
+	for i = 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+	if m > 0 && n > 0 {
+		dp[0][0] = 1
+	}
+	for i = 0; i < m; i++ {
+		for j = 0; j < n; j++ {
+			if obstacleGrid[i][j] == 1 {
+				dp[i][j] = 0
+				continue
+			}
+			if i > 0 && j > 0 {
+				dp[i][j] = dp[i-1][j] + dp[i][j-1]
+				continue
+			}
+			if i > 0 {
+				dp[i][j] = dp[i-1][j]
+				continue
+			}
+			if j > 0 {
+				dp[i][j] = dp[i][j-1]
+				continue
+			}
+		}
+	}
+	return dp[m-1][n-1]
+}
